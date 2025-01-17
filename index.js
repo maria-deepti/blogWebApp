5c59518170cba43acbfebbfb58dee0d7b69e8f37
Maria <mariadeeptitt@gmail.com>
2025-01-18 00:01:22 +0530

    initial setup done

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+// import bodyParser from "body-parser";
+
+const app = express();
+const port =3000;
+
+
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+})
+
+app.listen(port,() =>{
+    console.log(`listening on port ${port}`);
+})
