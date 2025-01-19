8840f7a02a2f218810f5ce10751efc1ccffb9150
Maria <mariadeeptitt@gmail.com>
2025-01-20 00:52:29 +0530

    first-version-scratch

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ import express from "express";
 
 const app = express();
 const port =3000;
-
-
+app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
     res.render("index.ejs");
+})
+
+app.get("/blog1",(req,res)=>{
+    res.render("pages/blog1.ejs");
+})
+
+app.get("/blog2",(req,res)=>{
+    res.render("pages/blog2.ejs");
 })
 
 app.listen(port,() =>{
